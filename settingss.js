f58d3b7bc87faa7b65845695ce58dabc6b07b6e0
samuelontsouka001-web <samuelontsouka001@gmail.com>
2025-08-15 19:53:21 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~cnNhXYQC#06fEKxddLSLLfISNFDwMLIBeMd6b0zUlza2u6ZZMLQw",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "242066184260", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
